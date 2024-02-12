78d2767b7fa1eadf1fb327a61ea3eab3d29de8ce
Prateek <prateek.9n@gmail.com>
2024-02-12 10:09:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG8051_Assignment_2_Prateek_chadha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
